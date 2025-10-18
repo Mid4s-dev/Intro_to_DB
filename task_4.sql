0466c24b2f644e9a350955d5e9f2a918dacabc23
mid4s-dev <lugayajoshua@gmail.com>
2025-10-19 02:55:19 +0300

    mega update

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,13 @@
 -- Script to print the full description of the books table
-SHOW COLUMNS FROM Books;
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
